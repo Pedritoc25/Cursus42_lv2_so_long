1f52ef4965cb49a1874724170082294c999abf32
Pedritoc25 <pedritoc25@gmail.com>
2024-10-08 12:56:26 +0200

    added sample maps and proyect structure upgrade

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,31 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:26:26 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/10/08 11:26:27 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:31:57 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*void parsing_map()
+{
+	all_collectables_collected
+	on_wall
+	map_valid
+	map_invalid
+	on_collectable
+	collectables
+	on exit
+	exits
+	replace_current_position_with_wall
+	one_of_the_four_adjacent_directions_is_posible
+}*/
+
+char** read_map()
+{
+	
+}
+
+
+int main(int argc, char **argv)
+{
+	
+}
