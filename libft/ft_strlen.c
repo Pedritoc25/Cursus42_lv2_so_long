5eba0c18136d10731f15dd3d37ad2a237a6f172f
Pedritoc25 <pedritoc25@gmail.com>
2024-10-17 22:40:12 +0200

    ft_strlen memory protected

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 17:04:18 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/02/08 15:41:26 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/10/17 22:18:43 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t	ft_strlen(const char *s)
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (s[len] != '\0')
 		len++;
 	return (len);
