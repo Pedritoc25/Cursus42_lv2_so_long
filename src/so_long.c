7fa8eb7e685d970632d92cabe880b4924faeda4a
Pedritoc25 <pedritoc25@gmail.com>
2024-10-29 17:26:55 +0100

    3th step. Working with maps

--- src/so_long.c
+++ src/so_long.c
@@ -6,83 +6,13 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:26:26 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/10/22 17:35:14 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/10/29 17:15:07 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-/*void parsing_map()
+int main (void)
 {
-	all_collectables_collected
-	on_wall
-	map_valid
-	map_invalid
-	on_collectable
-	collectables
-	on exit
-	exits
-	replace_current_position_with_wall
-	one_of_the_four_adjacent_directions_is_posible
-}*/
-
-/*char** read_map(char **map)
-{
-		get_next_line(map, 1);
-}*/
-
-size_t test( char *s)
-{
-	return (ft_strlen_gnl(s));
-}
-
-// Written by Bruh
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "../MLX42/include/MLX42/MLX42.h"
-#define WIDTH 256
-#define HEIGHT 256
-
-// Exit the program as failure.
-static void ft_error(void)
-{
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
-}
-
-// Print the window width and height.
-static void ft_hook(void* param)
-{
-	const mlx_t* mlx = param;
-
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
-}
-
-int32_t	main(void)
-{
-
-	// MLX allows you to define its core behaviour before startup.
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
-	if (!mlx)
-		ft_error();
-
-	/* Do stuff */
-
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+	return 0;
 }
