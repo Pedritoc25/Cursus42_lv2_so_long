78e74fcbc49dc047b609844347ccf1d72017fb10
Pedritoc25 <pedritoc25@gmail.com>
2025-01-27 13:57:03 +0100

    map reader created

--- src/map_structure/read_map.c
+++ src/map_structure/read_map.c
@@ -6,13 +6,24 @@
 /*   By: pcabanas <pcabanas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:09:57 by pcabanas          #+#    #+#             */
-/*   Updated: 2024/10/29 17:13:47 by pcabanas         ###   ########.fr       */
+/*   Updated: 2024/11/20 12:35:03 by pcabanas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//Here oes the GNL to read the map
+# include "../so_long.h"
+# include "map_structure.h"
 
-/*char** read_map(char **map)
+//check if map file ends in .ber
+void	ft_read_map(char *map_path)
 {
-		get_next_line(map, 1);
-}*/
+	size_t path_length;
+	
+	if (!map_path)
+		ft_print_error(0);
+	path_length = ft_strlen(map_path);
+	if (path_length < 4)
+		ft_print_error(0);
+	if (map_path[path_length - 4] != '.' || map_path[path_length - 3] != 'b'
+		|| map_path[path_length - 2] != 'e' || map_path[path_length - 1] != 'r')
+		ft_print_error(0);
+}
